3e94290d9980adb619acde82688419cc76e51a15
Evhen Nazarenko <Evgen1215@ukr.net>
2024-10-10 13:13:29 +0300

    ready all tasks

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,6 +1,7 @@
 // З масива людей(people) Вам потрібно знайти людину за іменем(name)
 function findByName(people, name) {
-  // Ваш код
+  let findByName = people.find((person) => person.name === name);
+  return findByName;
 }
 
 module.exports = findByName;
